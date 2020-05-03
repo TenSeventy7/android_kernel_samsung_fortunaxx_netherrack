4930178ebedbabb711afeffe8bd87b7fedf4bfc9
SparXFusion <s2234nadar@gmail.com>
2020-05-03 10:14:36 +0200

    [LOCAL ONLY] build: point toolchain to hardcoded directories

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# export ARCH=arm
+export CROSS_COMPILE=$HOME/bedrock/arm-eabi-4.7/bin/arm-eabi-
+
+export PATH="$HOME/bedrock/arm-eabi-4.7/bin/:$PATH"
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+export SUBARCH=arm
+
 mkdir output
 
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_fortuna3g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
---
+++ output/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Sun May 3 10:10:09 CEST 2020"
+#define LINUX_COMPILE_BY "HassanSardar"
+#define LINUX_COMPILE_HOST "New-Dimensions™"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
