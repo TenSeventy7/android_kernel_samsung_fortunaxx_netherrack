606cb9c8e6bcf184ee622404902a0af13cf4c8d5
John Vincent <git@tensevntysevn.xyz>
2020-05-06 10:17:33 +0800

    build: Use the newer defconfig

--- build_ve_kernel.sh
+++ build_ve_kernel.sh
@@ -9,7 +9,7 @@ export SUBARCH=arm
 
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_fortunave3g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_fortunave3g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
