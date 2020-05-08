3abb115298ebd61aebfd919d249fd40a5939a6f0
John Vincent <git@tensevntysevn.xyz>
2020-05-08 14:13:25 +0800

    build: Fully use device defconfig to build kernel
    Completes commit e86144bc13f133aef0584c2a8d504ee4b7919545.

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ export SUBARCH=arm
 
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_fortuna3g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_fortuna3g_eur_defconfig msm8916_fortuna3g_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
